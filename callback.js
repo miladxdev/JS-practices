473a0450d6378e8889a43d29008e0a8645edd336
Milad <miladxgh@outlook.com>
2021-05-22 15:01:04 +0430

    refactoring code

--- callback.js
+++ callback.js
@@ -1,12 +1,53 @@
-users = ['Milad', 'Masoud']; // Server
+// In JavaScript, a callback is a function passed into another function as an argument to be executed later.
+var users = ["Yara", "Ellie", "Joel"]; // Database
+
+function sendUser(username, callback) {
+
+    setTimeout(function() {
+        users.push(username);
+        callback();
+    }, 2000);
+}
+
+function getUsers() {
+    setTimeout(function() {
+        console.log(users);
+    }, 1000);
+}
+
+sendUser("Abbi", getUsers);
+
+
+// // this is not working
+
+// var users = ["Tommy", "Ellie", "Joel"];
+
+// function addUser(username) {
+//     setTimeout(function() {
+//         users.push(username);
+//     }, 2000);
+// }
+
+// function getUsers() {
+//     setTimeout(function() {
+//         console.log(users);
+//     }, 1000);
+// }
+
+// addUser("Abbi");
+
+// getUsers();
+
+
+server_user = ['Milad', 'Masoud']; // Server
 
 function addUser(name, callback) {
-    users.push(name);
+    server_user.push(name);
     callback();
 }
 
 function printUser() {
-    console.log(users);
+    console.log(server_user);
 }
 
 addUser('Ali', printUser);
--- function.js
+++ function.js
@@ -1,9 +1,49 @@
-function sayHi(fname, lname) {
+function sayHello(fname, lname) {
     console.log(`Hello ${fname} ${lname}. welcome back!`);
 }
 
-sayHi('Walter', 'White');
-sayHi('Jesse', 'Pinkman');
-sayHi('Gus', 'Fring');
+sayHello('Walter', 'White');
+console.log(sayHello.length); // 0 : number of the paramrters
+
+// the rest parameters
+function sumAll(...args) {
+    let sum = 0;
+    for (let arg of args) sum += arg;
+    return sum
+}
+
+console.log(sumAll(1, 5, 10, 4));
 
 
+// Named Function Expression NFE -------------------
+let sayHi = function func(who) { // func is the name
+  alert(`Hello, ${who}`);
+};
+// There are two special things about the name func, that are the reasons for it:
+// It allows the function to reference itself internally.
+// It is not visible outside of the function.
+
+
+// let login = function(name) {
+//     if (name) {
+//         console.log( `hello ${name}` );
+//     } else {
+//         login("guest");
+//     }
+// };
+// let welcome = login;
+// login = null;
+// welcome(); // Error, the nested sayHi call doesn't work any more!
+
+// FIX
+let login = function nested_login(name) {
+    if (name) {
+        console.log( `hello ${name}` );
+    } else {
+        nested_login("guest"); // nested function
+    }
+};
+
+let welcome = login;
+login = null;
+welcome(); // works
