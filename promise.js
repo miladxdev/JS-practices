d2af5267694a96def93cb083f011f4ee9c06649d
Milad <miladxgh@outlook.com>
2021-06-16 00:16:44 +0430

    async-await

--- promise.js
+++ promise.js
@@ -13,63 +13,61 @@
 //     // console.log('This is in catch ' + message);
 // });
 
-
 // ES5: Full example
 
 var isMomHappy = true;
 
 // Promise
-var willIGetNewPhone = new Promise(
-    function (resolve, reject) {
-        if (isMomHappy) {
-            var phone = {
-                brand: 'iPhone',
-                color: 'gold'
-            };
-            resolve(phone); // fulfilled
-        } else {
-            var reason = new Error('mom is not happy');
-            reject(reason); // reject
-        }
-
-    }
-);
+var willIGetNewPhone = new Promise(function (resolve, reject) {
+  if (isMomHappy) {
+    var phone = {
+      brand: "iPhone",
+      color: "gold",
+    };
+    resolve(phone); // fulfilled
+  } else {
+    var reason = new Error("mom is not happy");
+    reject(reason); // reject
+  }
+});
 
 // 2nd promise
 var showOff = function (phone) {
-    var message = 'Hey friend, I have a new ' +
-        phone.color + ' ' + phone.brand + ' phone';
+  var message = "Hey friend, I have a new " + phone.color + " " + phone.brand + " phone";
 
-    return Promise.resolve(message);
+  return Promise.resolve(message);
 };
 
 // call our promise
 var askMom = function () {
-    willIGetNewPhone.then(showOff) // chain it here
-        .then(function (fulfilled) {
-            console.log(fulfilled);
-            // output: 'Hey friend, I have a new black Samsung phone.'
-        })
-        .catch(function (error) {
-            // oops, mom don't buy it
-            console.log(error.message);
-            // output: 'mom is not happy'
-        });
+  willIGetNewPhone
+    .then(showOff) // chain it here
+    .then(function (fulfilled) {
+      console.log(fulfilled);
+      // output: 'Hey friend, I have a new black Samsung phone.'
+    })
+    .catch(function (error) {
+      // oops, mom don't buy it
+      console.log(error.message);
+      // output: 'mom is not happy'
+    });
 };
 
 // askMom();
 
-
 let goingToBeach = new Promise(function (resolve, reject) {
-    let weather = 'rainy';
-    if (weather == 'sunny') {
-        resolve("we are going...");
-    } else {
-        reject(new Error(`sorry its ${weather}!`));
-    }
+  let weather = "rainy";
+
+  if (weather === "sunny") {
+    resolve("we are going...");
+  } else {
+    reject(new Error(`sorry its ${weather}!`));
+  }
 });
 
-goingToBeach.then(
-    result => console.log(result),
-    error => console.log(error.message)
-);
+goingToBeach
+  .then(
+    (result) => console.log(result),
+    (error) => console.log(error.message)
+  )
+  .catch((err) => console.log(err.message));
